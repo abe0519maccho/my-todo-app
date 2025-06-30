c1333af03f82f5a122022e8345f151a4e05b911a
0278 ASN <h_abe@a-ssign.com>
2025-06-30 17:47:59 +0900

    タイトルを更新

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,8 @@
+// src/lib/supabaseClient.ts
+import { createClient } from '@supabase/supabase-js'
+
+export const createBrowserSupabaseClient = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  return createClient(supabaseUrl, supabaseKey)
+}
